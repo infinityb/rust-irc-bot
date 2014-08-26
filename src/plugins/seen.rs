05dffa0a32c1a6274b76aeaca938860d5ca52b4e
Stacey Ell <stacey.ell@gmail.com>
2014-08-26 17:50:41 -0600

    UI updates for seen command

--- src/plugins/seen.rs
+++ src/plugins/seen.rs
@@ -55,12 +55,60 @@ fn trim_vec<T>(vec: Vec<T>) -> Vec<T> {
 }
 
 
+fn is_message_interesting(message: &IrcMessage) -> bool {
+    match message.command() {
+        "PRIVMSG" => true,
+        "QUIT" => true,
+        _ => false
+    }
+}
+
+
+fn format_activity(nick: &str, records: &Vec<SeenRecord>) -> String {
+    let mut user_has_quit: Option<Timespec> = None;
+    let mut prev_message: Option<&SeenRecord> = None;
+
+    for record in records.iter().rev() {
+        if record.message.command() == "QUIT" {
+            user_has_quit = Some(record.when.clone());
+        }
+        if record.message.is_privmsg() {
+            prev_message = Some(record);
+            break;
+        }
+    }
+    let now = get_time();
+    match (user_has_quit, prev_message) {
+        (Some(when_quit), Some(record)) => {
+            let seconds = (now - record.when).sec;
+            let quit_seconds = (when_quit - record.when).sec;
+            format!("{} said ``{}'' {} seconds ago before quitting {} seconds later",
+                nick, record.message.get_arg(1), seconds, quit_seconds)
+        },
+        (None, Some(record)) => {
+            // let message_sent = format_message_sent(message);
+            let seconds = (now - record.when).sec;
+            format!("{} said ``{}'' {} seconds ago", nick, record.message.get_arg(1), seconds)
+        },
+        (Some(when_quit), None) => {
+            format!("{} quit {} seconds ago", nick, (now - when_quit).sec)
+        },
+        (None, None) => {
+            format!("Sorry, I am very confused about {}", nick)
+        }
+    }
+}
+
+
 impl RustBotPlugin for SeenPlugin {
     fn configure(&mut self, conf: &mut IrcBotConfigurator) {
         conf.map("seen");
     }
 
     fn accept(&mut self, m: &CommandMapperDispatch, message: &IrcMessage) {
+        if !is_message_interesting(message) {
+            return;
+        }
         let source_nick = match message.source_nick() {
             Some(source_nick) => source_nick,
             None => return
@@ -88,22 +136,25 @@ impl RustBotPlugin for SeenPlugin {
         let args = message.get_args();
         if args.len() != 2 {
             // Invalid PRIVMSG
+            return;
         }
 
         let args: Vec<&str> = args[1].as_slice().splitn(1, ' ').collect();
         if args.len() != 2 {
             // Invalid command
+            return;
         }
 
         let target_nick = String::from_str(args[1]);
-        match self.map.find(&target_nick) {
-            Some(val) => {
-                m.reply(format!("{} has {} activity records", args[1], val.len()));
-            },
+
+        let activity = match self.map.find(&target_nick) {
+            Some(val) => val,
             None => {
                 m.reply(format!("{} is unknown", args[1]));
+                return
             }
-        }
-        
+        };
+        let activity_formatted = format_activity(target_nick.as_slice(), activity);
+        m.reply(activity_formatted);
     }
 }
