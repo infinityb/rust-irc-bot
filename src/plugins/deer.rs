1f6bf9a4a542d840e652d01a3661428ed49798ed
Stacey Ell <stacey.ell@gmail.com>
2014-08-28 21:29:18 -0600

    Add throttle to deer

--- src/plugins/deer.rs
+++ src/plugins/deer.rs
@@ -10,6 +10,7 @@ use std::io::TcpStream;
 use std::str::replace;
 use serialize::json;
 use serialize::json::{DecodeResult, ApplicationError};
+use time::{get_time, Timespec};
 
 
 static DEER: &'static str = concat!(
@@ -46,8 +47,9 @@ fn get_deer(deer_name: &str) -> DecodeResult<DeerApiResponse> {
     let deer_name = replace(deer_name, " ", "%20");
 
     assert!(stream.write(format!(
-        "GET /deerlist.php?deer={} HTTP/1.0\r\nHost: deer.satf.se\r\n\r\n",
-        deer_name.as_slice()
+        "GET /deerlist.php?deer={} HTTP/1.0\r\nHost: {}\r\n\r\n",
+        deer_name.as_slice(),
+        HTTP_SERVER
     ).as_bytes()).is_ok());
 
     let http_doc = match stream.read_to_string() {
@@ -78,10 +80,88 @@ impl DeerPlugin {
     }
 }
 
+struct DeerInternalState {
+    lines_sent: u64,
+    last_request: Option<Timespec>
+}
+
+
+impl DeerInternalState {
+    fn new() -> DeerInternalState {
+        DeerInternalState {
+            lines_sent: 0,
+            last_request: None
+        }
+    }
+
+    fn throttle(&mut self) -> bool {
+        let now = get_time();
+
+        let (new_last_request, throttle_ok) = match self.last_request {
+            Some(last_request) => {
+                if (now - last_request).sec < 60 {
+                    (Some(last_request), true)
+                } else {
+                    (Some(now), false)
+                }                
+            },
+            None => (Some(now), false)
+        };
+        self.last_request = new_last_request;
+        throttle_ok
+    }
+
+    fn start(&mut self, rx: Receiver<(CommandMapperDispatch, IrcMessage)>) {
+        for (m, message) in rx.iter() {
+            if message.get_args().len() < 2 {
+                continue
+            }
+            let command_data = match parse_command(&m, &message) {
+                Some(DeerStats) => Some(DeerStats),
+                Some(command_data) => {
+                    if self.throttle() {
+                        m.reply(String::from_str("2deer4plus"));
+                        None
+                    } else {
+                        Some(command_data)
+                    }
+                },
+                None => None
+            };
+            match command_data {
+                Some(Deer) => {
+                    for deer_line in DEER.split('\n') {
+                        m.reply(String::from_str(deer_line));
+                        self.lines_sent += 1;
+                    }
+                },
+                Some(Deerkins(deer_name)) => {
+                    match get_deer(deer_name) {
+                        Ok(deer_data) => {
+                            for deer_line in deer_data.irccode.as_slice().split('\n') {
+                                m.reply(String::from_str(deer_line));
+                                self.lines_sent += 1;
+                            }
+                        },
+                        Err(err) => {
+                            m.reply(format!("error: {}", err));
+                        }
+                    } 
+                },
+                Some(DeerStats) => {
+                    m.reply(format!("lines sent: {}", self.lines_sent));
+                }
+                None => continue
+            };
+        }
+    }
+}
+
 
 enum DeerCommandType<'a> {
     Deer,
-    Deerkins(&'a str)
+    Deerkins(&'a str),
+    DeerStats
 }
 
 
@@ -98,6 +178,7 @@ fn parse_command<'a>(m: &CommandMapperDispatch, message: &'a IrcMessage) -> Opti
     match m.command() {
         Some("deer") => Some(Deer),
         Some("deerkins") => parse_deerkins(message),
+        Some("deerstats") => Some(DeerStats),
         Some(_) => None,
         None => None
     }
@@ -107,6 +188,7 @@ fn parse_command<'a>(m: &CommandMapperDispatch, message: &'a IrcMessage) -> Opti
 impl RustBotPlugin for DeerPlugin {
     fn configure(&mut self, conf: &mut IrcBotConfigurator) {
         conf.map("deer");
+        conf.map("deerstats");
         conf.map("deerkins");
     }
 
@@ -115,31 +197,8 @@ impl RustBotPlugin for DeerPlugin {
         self.sender = Some(tx);
 
         spawn(proc() {
-            for (m, message) in rx.iter() {
-                if message.get_args().len() < 2 {
-                    continue
-                }
-                match parse_command(&m, &message) {
-                    Some(Deer) => {
-                        for deer_line in DEER.split('\n') {
-                            m.reply(String::from_str(deer_line));
-                        }
-                    },
-                    Some(Deerkins(deer_name)) => {
-                        match get_deer(deer_name) {
-                            Ok(deer_data) => {
-                                for deer_line in deer_data.irccode.as_slice().split('\n') {
-                                    m.reply(String::from_str(deer_line));
-                                }
-                            },
-                            Err(err) => {
-                                m.reply(format!("error: {}", err));
-                            }
-                        }
-                    },
-                    None => continue
-                };
-            }
+            let mut deer_internal_state = DeerInternalState::new();
+            deer_internal_state.start(rx);
         });
     }
 
